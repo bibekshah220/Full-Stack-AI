f599632a908fb8ab4fefb252c5f0b6f45418cebe
bibekshah220 <bibekshah425@gmail.com>
2026-03-14 00:01:42 +0545

    nested conditions eg

--- day1.py
+++ day1.py
@@ -6,3 +6,12 @@ elif num == 5:
     print("five")
 else:
     print("Negative number")
+
+# Example 2: nested conditions
+age = 25
+if age > 18:
+    if age < 30:
+        print("You are a young adult.")
+    else:
+        print("You are an adult.")
+    
