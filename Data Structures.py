4904a1f6c9b6734ffe5b30101c5fb854fa346210
bibekshah220 <bibekshah425@gmail.com>
2026-03-26 21:20:42 +0545

    modifying list elements

--- Data Structures.py
+++ Data Structures.py
@@ -11,3 +11,9 @@ mixed_list = [1, 'hello', 3.14, True]
 print(numbers[0])  # Output: 1
 print(fruits[1])  # Output: 'banana'
 print(mixed_list[2])  # Output: 3.14
+
+# modifying list elements
+numbers[0] = 10 
+print(numbers)  # Output: [10, 2, 3, 4, 5]
+fruits.append('orange')
+print(fruits)  # Output: ['apple', 'banana', 'cherry', 'orange']
