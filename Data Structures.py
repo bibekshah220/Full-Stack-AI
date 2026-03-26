2cc9ff6c58629a9fc6239bde54cbaa7fadfb3ef0
bibekshah220 <bibekshah425@gmail.com>
2026-03-26 21:19:00 +0545

    Add Data Structures module with examples of list creation and usage

---
+++ Data Structures.py
@@ -0,0 +1,8 @@
+# what is list
+# list is a collection of items in a particular order. It is a mutable data structure, which means that you can change the contents of a list after it has been created. Lists are defined by enclosing a comma-separated sequence of items in square brackets [].
+
+# creating a list
+numbers = [1, 2, 3, 4, 5]
+fruits = ['apple', 'banana', 'cherry']
+
+mixed_list = [1, 'hello', 3.14, True]
