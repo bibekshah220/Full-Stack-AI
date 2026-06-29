34d48d9a4cb67583a5613fe7e078772bcf9b2f79
bibekshah220 <bibekshah425@gmail.com>
2026-06-29 21:38:51 +0545

    Add string formatting examples and common string methods demonstration

--- string.py
+++ string.py
@@ -2,5 +2,21 @@
 first = "bibek"
 last = "shah"
 full = first + " " + last
-print(full)
+# print(full)
+
+text = "Hello, World!"
+print(text[0])  # Output: 'H'
+print(text[7])  # Output: 'W'
+
+#formatting strings
+name = "bibek"
+age = 25
+print("My name is {} and I am {} years old.".format(name, age))  # Output: My name is bibek and I am 25 years old.
+print(f"My name is {name} and I am {age} years old.")  #Output: My name is bibek and I am 25 years old.
+
+#commom string methods
+text = "Hello, World!"
+split_text = text.split(",")  # splits the string into a list of substrings based on the comma delimiter
+print(split_text)  # Output: ['Hello', ' World!']
+
 
